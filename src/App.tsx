a15e084e532ca31b7bbb02521b6dd7413bc0ee49
Rhinoguana <57597522+Rhinoguana@users.noreply.github.com>
2025-01-18 16:32:42 -0500

    Completed Lab1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import Labs from "./Labs"
+import Labs from "./Labs";
+import { HashRouter, Route, Routes, Navigate } from 'react-router-dom';
 
-function App() {
+export default function App() {
   return (
-    <div>
-      <Labs />
-    </div>
-  )
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="Labs" />}/>
+          <Route path="/Labs/*" element={<Labs />} />
+        </Routes>
+      </div>
+    </HashRouter>
+  );
 }
-
-export default App
