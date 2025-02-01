0aea6232a8ee3e20c6a875d308dc1d3b088dba0c
Rhinoguana <57597522+Rhinoguana@users.noreply.github.com>
2025-01-31 23:11:11 -0500

    Completed Lab2

---
+++ src/Labs/Lab2/Float.tsx
@@ -0,0 +1,141 @@
+export default function Float() {
+    return (
+        <div id="wd-float-divs">
+            <h2>Float</h2>
+            <div>
+                <img className="wd-float-right"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed imperdiet sapien. Nulla eu tellus
+                vitae massa cursus congue. Sed tellus libero, porttitor at ex sed, rhoncus elementum dolor. Integer
+                eleifend non ante non imperdiet. Donec eleifend tristique nulla et ultricies. Nam nec malesuada ipsum,
+                in efficitur nulla. Etiam at pretium odio. Quisque quam dui, blandit ut feugiat elementum, suscipit
+                non leo. Nam sapien lorem, tempus et est sed, efficitur pellentesque enim. Phasellus sit amet nisl ac
+                odio lacinia tempor. Fusce tincidunt enim justo, nec hendrerit turpis facilisis ac. Aliquam erat volutpat.
+                In vel lacus arcu. Phasellus nec odio ac leo finibus vulputate. Etiam tristique vestibulum purus non
+                luctus. Sed vel facilisis sapien. Mauris hendrerit nulla eu blandit rhoncus. Sed vel volutpat lectus.
+                Maecenas ultricies leo sapien, eget posuere nisi fermentum vitae. Integer sollicitudin ut urna non
+                sollicitudin. Ut vitae convallis nisi. Aenean iaculis felis nec fringilla sagittis. Donec non nisi est.
+                Ut velit leo, bibendum vel scelerisque ut, lacinia in orci. Suspendisse hendrerit sollicitudin molestie.
+                Quisque scelerisque, neque at convallis finibus, ex nulla consequat tellus, quis aliquet elit est eu quam.
+                Etiam eu convallis justo. Ut interdum nisl cursus purus laoreet, ut efficitur leo scelerisque. Mauris
+                elementum velit erat. Etiam posuere justo id egestas hendrerit. Aenean et urna sem. Mauris suscipit metus
+                vitae metus dictum, pharetra feugiat mi dapibus. Nunc vel erat metus. Pellentesque in ligula mollis,
+                sollicitudin elit nec, finibus nulla. Praesent vitae ipsum a ligula eleifend vehicula. Ut condimentum
+                non neque non laoreet. Curabitur venenatis finibus diam vitae accumsan. Donec congue velit ac lorem molestie
+                fermentum. Donec ultrices odio justo, vel elementum nulla convallis ut. Ut molestie justo in eros pharetra,
+                sed tempor ipsum molestie. Donec eu molestie nisi, quis porta ipsum. Fusce eu egestas libero, porttitor
+                ornare risus. Nullam eleifend nulla felis, a laoreet urna auctor sed. In nec mi odio. Nullam felis elit,
+                sollicitudin quis aliquet vitae, vehicula eu diam. Morbi consequat at est at hendrerit. Nam tempus arcu ut
+                vulputate placerat. Aenean interdum leo a dui ultrices elementum. Curabitur porttitor commodo massa. Etiam
+                tempus convallis vestibulum. In hendrerit auctor efficitur. Praesent varius, elit non euismod condimentum,
+                nunc elit volutpat ipsum, et lobortis est est at odio. Vivamus mattis rutrum condimentum. Donec non odio
+                eget nulla interdum iaculis vel vel nisi. Vestibulum ac ligula suscipit, maximus lacus vel, pellentesque lectus.
+                Nulla ut dui sed risus dignissim maximus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+                inceptos himenaeos. Nunc pellentesque ipsum quis nisl volutpat tincidunt. Proin sed nulla sem. Nunc imperdiet
+                orci eu nulla sagittis mollis. Mauris lacinia at nibh eu vestibulum.
+                <img className="wd-float-left"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed imperdiet sapien. Nulla eu tellus
+                vitae massa cursus congue. Sed tellus libero, porttitor at ex sed, rhoncus elementum dolor. Integer
+                eleifend non ante non imperdiet. Donec eleifend tristique nulla et ultricies. Nam nec malesuada ipsum,
+                in efficitur nulla. Etiam at pretium odio. Quisque quam dui, blandit ut feugiat elementum, suscipit
+                non leo. Nam sapien lorem, tempus et est sed, efficitur pellentesque enim. Phasellus sit amet nisl ac
+                odio lacinia tempor. Fusce tincidunt enim justo, nec hendrerit turpis facilisis ac. Aliquam erat volutpat.
+                In vel lacus arcu. Phasellus nec odio ac leo finibus vulputate. Etiam tristique vestibulum purus non
+                luctus. Sed vel facilisis sapien. Mauris hendrerit nulla eu blandit rhoncus. Sed vel volutpat lectus.
+                Maecenas ultricies leo sapien, eget posuere nisi fermentum vitae. Integer sollicitudin ut urna non
+                sollicitudin. Ut vitae convallis nisi. Aenean iaculis felis nec fringilla sagittis. Donec non nisi est.
+                Ut velit leo, bibendum vel scelerisque ut, lacinia in orci. Suspendisse hendrerit sollicitudin molestie.
+                Quisque scelerisque, neque at convallis finibus, ex nulla consequat tellus, quis aliquet elit est eu quam.
+                Etiam eu convallis justo. Ut interdum nisl cursus purus laoreet, ut efficitur leo scelerisque. Mauris
+                elementum velit erat. Etiam posuere justo id egestas hendrerit. Aenean et urna sem. Mauris suscipit metus
+                vitae metus dictum, pharetra feugiat mi dapibus. Nunc vel erat metus. Pellentesque in ligula mollis,
+                sollicitudin elit nec, finibus nulla. Praesent vitae ipsum a ligula eleifend vehicula. Ut condimentum
+                non neque non laoreet. Curabitur venenatis finibus diam vitae accumsan. Donec congue velit ac lorem molestie
+                fermentum. Donec ultrices odio justo, vel elementum nulla convallis ut. Ut molestie justo in eros pharetra,
+                sed tempor ipsum molestie. Donec eu molestie nisi, quis porta ipsum. Fusce eu egestas libero, porttitor
+                ornare risus. Nullam eleifend nulla felis, a laoreet urna auctor sed. In nec mi odio. Nullam felis elit,
+                sollicitudin quis aliquet vitae, vehicula eu diam. Morbi consequat at est at hendrerit. Nam tempus arcu ut
+                vulputate placerat. Aenean interdum leo a dui ultrices elementum. Curabitur porttitor commodo massa. Etiam
+                tempus convallis vestibulum. In hendrerit auctor efficitur. Praesent varius, elit non euismod condimentum,
+                nunc elit volutpat ipsum, et lobortis est est at odio. Vivamus mattis rutrum condimentum. Donec non odio
+                eget nulla interdum iaculis vel vel nisi. Vestibulum ac ligula suscipit, maximus lacus vel, pellentesque lectus.
+                Nulla ut dui sed risus dignissim maximus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+                inceptos himenaeos. Nunc pellentesque ipsum quis nisl volutpat tincidunt. Proin sed nulla sem. Nunc imperdiet
+                orci eu nulla sagittis mollis. Mauris lacinia at nibh eu vestibulum.
+                <img className="wd-float-right"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed imperdiet sapien. Nulla eu tellus
+                vitae massa cursus congue. Sed tellus libero, porttitor at ex sed, rhoncus elementum dolor. Integer
+                eleifend non ante non imperdiet. Donec eleifend tristique nulla et ultricies. Nam nec malesuada ipsum,
+                in efficitur nulla. Etiam at pretium odio. Quisque quam dui, blandit ut feugiat elementum, suscipit
+                non leo. Nam sapien lorem, tempus et est sed, efficitur pellentesque enim. Phasellus sit amet nisl ac
+                odio lacinia tempor. Fusce tincidunt enim justo, nec hendrerit turpis facilisis ac. Aliquam erat volutpat.
+                In vel lacus arcu. Phasellus nec odio ac leo finibus vulputate. Etiam tristique vestibulum purus non
+                luctus. Sed vel facilisis sapien. Mauris hendrerit nulla eu blandit rhoncus. Sed vel volutpat lectus.
+                Maecenas ultricies leo sapien, eget posuere nisi fermentum vitae. Integer sollicitudin ut urna non
+                sollicitudin. Ut vitae convallis nisi. Aenean iaculis felis nec fringilla sagittis. Donec non nisi est.
+                Ut velit leo, bibendum vel scelerisque ut, lacinia in orci. Suspendisse hendrerit sollicitudin molestie.
+                Quisque scelerisque, neque at convallis finibus, ex nulla consequat tellus, quis aliquet elit est eu quam.
+                Etiam eu convallis justo. Ut interdum nisl cursus purus laoreet, ut efficitur leo scelerisque. Mauris
+                elementum velit erat. Etiam posuere justo id egestas hendrerit. Aenean et urna sem. Mauris suscipit metus
+                vitae metus dictum, pharetra feugiat mi dapibus. Nunc vel erat metus. Pellentesque in ligula mollis,
+                sollicitudin elit nec, finibus nulla. Praesent vitae ipsum a ligula eleifend vehicula. Ut condimentum
+                non neque non laoreet. Curabitur venenatis finibus diam vitae accumsan. Donec congue velit ac lorem molestie
+                fermentum. Donec ultrices odio justo, vel elementum nulla convallis ut. Ut molestie justo in eros pharetra,
+                sed tempor ipsum molestie. Donec eu molestie nisi, quis porta ipsum. Fusce eu egestas libero, porttitor
+                ornare risus. Nullam eleifend nulla felis, a laoreet urna auctor sed. In nec mi odio. Nullam felis elit,
+                sollicitudin quis aliquet vitae, vehicula eu diam. Morbi consequat at est at hendrerit. Nam tempus arcu ut
+                vulputate placerat. Aenean interdum leo a dui ultrices elementum. Curabitur porttitor commodo massa. Etiam
+                tempus convallis vestibulum. In hendrerit auctor efficitur. Praesent varius, elit non euismod condimentum,
+                nunc elit volutpat ipsum, et lobortis est est at odio. Vivamus mattis rutrum condimentum. Donec non odio
+                eget nulla interdum iaculis vel vel nisi. Vestibulum ac ligula suscipit, maximus lacus vel, pellentesque lectus.
+                Nulla ut dui sed risus dignissim maximus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+                inceptos himenaeos. Nunc pellentesque ipsum quis nisl volutpat tincidunt. Proin sed nulla sem. Nunc imperdiet
+                orci eu nulla sagittis mollis. Mauris lacinia at nibh eu vestibulum.
+                <img className="wd-float-left"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed imperdiet sapien. Nulla eu tellus
+                vitae massa cursus congue. Sed tellus libero, porttitor at ex sed, rhoncus elementum dolor. Integer
+                eleifend non ante non imperdiet. Donec eleifend tristique nulla et ultricies. Nam nec malesuada ipsum,
+                in efficitur nulla. Etiam at pretium odio. Quisque quam dui, blandit ut feugiat elementum, suscipit
+                non leo. Nam sapien lorem, tempus et est sed, efficitur pellentesque enim. Phasellus sit amet nisl ac
+                odio lacinia tempor. Fusce tincidunt enim justo, nec hendrerit turpis facilisis ac. Aliquam erat volutpat.
+                In vel lacus arcu. Phasellus nec odio ac leo finibus vulputate. Etiam tristique vestibulum purus non
+                luctus. Sed vel facilisis sapien. Mauris hendrerit nulla eu blandit rhoncus. Sed vel volutpat lectus.
+                Maecenas ultricies leo sapien, eget posuere nisi fermentum vitae. Integer sollicitudin ut urna non
+                sollicitudin. Ut vitae convallis nisi. Aenean iaculis felis nec fringilla sagittis. Donec non nisi est.
+                Ut velit leo, bibendum vel scelerisque ut, lacinia in orci. Suspendisse hendrerit sollicitudin molestie.
+                Quisque scelerisque, neque at convallis finibus, ex nulla consequat tellus, quis aliquet elit est eu quam.
+                Etiam eu convallis justo. Ut interdum nisl cursus purus laoreet, ut efficitur leo scelerisque. Mauris
+                elementum velit erat. Etiam posuere justo id egestas hendrerit. Aenean et urna sem. Mauris suscipit metus
+                vitae metus dictum, pharetra feugiat mi dapibus. Nunc vel erat metus. Pellentesque in ligula mollis,
+                sollicitudin elit nec, finibus nulla. Praesent vitae ipsum a ligula eleifend vehicula. Ut condimentum
+                non neque non laoreet. Curabitur venenatis finibus diam vitae accumsan. Donec congue velit ac lorem molestie
+                fermentum. Donec ultrices odio justo, vel elementum nulla convallis ut. Ut molestie justo in eros pharetra,
+                sed tempor ipsum molestie. Donec eu molestie nisi, quis porta ipsum. Fusce eu egestas libero, porttitor
+                ornare risus. Nullam eleifend nulla felis, a laoreet urna auctor sed. In nec mi odio. Nullam felis elit,
+                sollicitudin quis aliquet vitae, vehicula eu diam. Morbi consequat at est at hendrerit. Nam tempus arcu ut
+                vulputate placerat. Aenean interdum leo a dui ultrices elementum. Curabitur porttitor commodo massa. Etiam
+                tempus convallis vestibulum. In hendrerit auctor efficitur. Praesent varius, elit non euismod condimentum,
+                nunc elit volutpat ipsum, et lobortis est est at odio. Vivamus mattis rutrum condimentum. Donec non odio
+                eget nulla interdum iaculis vel vel nisi. Vestibulum ac ligula suscipit, maximus lacus vel, pellentesque lectus.
+                Nulla ut dui sed risus dignissim maximus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+                inceptos himenaeos. Nunc pellentesque ipsum quis nisl volutpat tincidunt. Proin sed nulla sem. Nunc imperdiet
+                orci eu nulla sagittis mollis. Mauris lacinia at nibh eu vestibulum.
+                <div className="wd-float-done"></div>
+                <div>
+                    <div className="wd-float-left wd-dimension-portrait wd-bg-color-yellow">
+                        Yellow </div>
+                    <div className="wd-float-left wd-dimension-portrait wd-bg-color-blue wd-fg-color-white">
+                        Blue </div>
+                    <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
+                        Red </div>
+                    <img className="wd-float-right"
+                        src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+                    <div className="wd-float-done"></div>
+                </div>
+            </div>
+        </div>
+    );
+}
