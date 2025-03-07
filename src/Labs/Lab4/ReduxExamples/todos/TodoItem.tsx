34ba554ea45b6019851246d62633a24b4e7296d1
Rhinoguana <57597522+Rhinoguana@users.noreply.github.com>
2025-03-06 22:42:38 -0500

    Fixed button order for edit and delete

--- src/Labs/Lab4/ReduxExamples/todos/TodoItem.tsx
+++ src/Labs/Lab4/ReduxExamples/todos/TodoItem.tsx
@@ -5,10 +5,10 @@ export default function TodoItem({ todo, }) {
     const dispatch = useDispatch();
     return (
         <ListGroup.Item key={todo.id}>
-            <Button className="btn-primary me-2 float-end" onClick={() => dispatch(setTodo(todo))}
-                id="wd-set-todo-click"> Edit </Button>
             <Button className="btn-danger me-2 float-end" onClick={() => dispatch(deleteTodo(todo.id))}
                 id="wd-delete-todo-click"> Delete </Button>
+            <Button className="btn-primary me-2 float-end" onClick={() => dispatch(setTodo(todo))}
+                id="wd-set-todo-click"> Edit </Button>
             {todo.title}
         </ListGroup.Item>
     );
