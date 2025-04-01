152c3f4b87c26ac5d1ac83297121c8b490444f2a
Rhinoguana <57597522+Rhinoguana@users.noreply.github.com>
2025-04-01 18:17:07 -0400

    Finished Lab5

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -23,6 +23,10 @@ export default function TOC() {
           active={pathname.includes("Lab4")}>Lab 4</Nav.Link>
       </Nav.Item>
       <Nav.Item>
+        <Nav.Link as={Link} to="/Labs/Lab5" id="wd-a4"
+          active={pathname.includes("Lab5")}>Lab 5</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
         <Nav.Link as={Link} to="/Kambaz" id="wd-kambaz">Kambaz</Nav.Link>
       </Nav.Item>
       <Nav.Item>
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -2,6 +2,7 @@ import Lab1 from "./Lab1";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
 import Lab4 from "./Lab4";
+import Lab5 from "./Lab5";
 import { Route, Routes, Navigate } from "react-router";
 import TOC from "./TOC";
 import store from "./store";
@@ -10,17 +11,18 @@ import { Provider } from "react-redux";
 export default function Labs() {
     return (
         <Provider store={store}>
-        <div id="wd-labs" className="container-fluid">
-            <h1>Labs - Ryan San Jose</h1>
-            <TOC />
-            <Routes>
-                <Route path="/" element={<Navigate to="Lab1" />} />
-                <Route path="Lab1" element={<Lab1 />} />
-                <Route path="Lab2/*" element={<Lab2 />} />
-                <Route path="Lab3/*" element={<Lab3 />} />
-                <Route path="Lab4/*" element={<Lab4 />} />
-            </Routes>
-        </div>
+            <div id="wd-labs" className="container-fluid">
+                <h1>Labs - Ryan San Jose</h1>
+                <TOC />
+                <Routes>
+                    <Route path="/" element={<Navigate to="Lab1" />} />
+                    <Route path="Lab1" element={<Lab1 />} />
+                    <Route path="Lab2/*" element={<Lab2 />} />
+                    <Route path="Lab3/*" element={<Lab3 />} />
+                    <Route path="Lab4/*" element={<Lab4 />} />
+                    <Route path="Lab5/*" element={<Lab5 />} />
+                </Routes>
+            </div>
         </Provider>
     );
 }
